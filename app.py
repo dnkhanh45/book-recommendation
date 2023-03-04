705788e29abdb51d4a0d311e0ff209c59b4eac52
dnkhanh45 <yoshokarui@gmail.com>
2023-03-04 17:07:13 +0700

    Plot data

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/get", methods=['GET'])
+def get():
+    if request.method == 'GET':
+        print(request.args.get('user_id'))
+        response = jsonify({
+            "book_ids": [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        })
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+
+if __name__ == '__main__':
+    app.run(debug=True)
