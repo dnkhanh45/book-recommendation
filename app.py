ac3f9998bf4fc225b8bd1c381674b301e8905138
dnkhanh45 <yoshokarui@gmail.com>
2023-03-10 22:08:09 +0700

    finish 2 api

--- app.py
+++ app.py
@@ -7,15 +7,25 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/recommend", methods=['GET'])
-def recommend():
+@app.route("/matrix-factorization", methods=['GET'])
+def matrix_factorization():
     if request.method == 'GET':
         user_id = request.args.get('user_id')
         response = jsonify({
-            "book_ids": np.load('./recommend/{}.npy'.format(user_id)).tolist()
+            "book_ids": np.load('./recommend/matrix-factorization/{}.npy'.format(user_id)).tolist()
+        })
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+
+@app.route("/content-based", methods=['GET'])
+def content_based():
+    if request.method == 'GET':
+        user_id = request.args.get('user_id')
+        response = jsonify({
+            "book_ids": np.load('./recommend/content-based/{}.npy'.format(user_id)).tolist()
         })
         response.headers.add('Access-Control-Allow-Origin', '*')
         return response
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
